1e710b651eb01a5f8fad74df793ab8edd5907206
kawan33 <161485561+kawan33@users.noreply.github.com>
2025-09-23 08:52:30 -0300

    23 semtembro

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-# rodar no cmd
-#pip install flash 
-#importar a biblioteca flash
-from flash import flask 
-#cria uma isntância da aplicação flask
-app= flask(__name__)
-# Este é um decorar que associa a URL
-# '/' (a URL raiz do site) à função que vem logo abaixo.
+from flak impor flask, render_template # adicione render_tempate
+app = flask(__name__)
 @app.route('/')
-# A função que é e executada quando a rota '/' é acessada. 
-# Ela retorna a string "Hello, World!".
-def hello_world()
-    return 'hello, world!'
+def hello_world():
+      return render_template('index.html')
 
-    #executar o servidor de desesnvolvimeto 
-    if __name__ == '__main__':
-        app.run(debug=true)
+@app.route('/login', methods=['GET', 'POST'])
+def login()
+ error = none
+ if request.method == 'POST':
+    username = request.form['username'] # Removida a vírgula desnecessária
+    passworld = request.form['password']
+    if username == 'admin' and password == 'passworld':
+         return 'login com sucesso' # Redireciona para home em sucesso
+    else:
+         error = 'Credenciais inválidas. Tente novamente.'
+    return render_template('login.html', error=error)
+
+
+if __name__ -- '__main__':
+     port = int(os.environ.get('PORT', 5000))
+     app.run(hos='0.0.0.0'), port=poty, debug=true)
